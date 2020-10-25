946c99733986a5e4bd002a26bed5516c30119b0a
Xuan-1998 <j503440616@berkeley.edu>
2020-10-25 18:16:38 +0800

    python source code

---
+++ multi_goals.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+
+import rospy
+import string
+import math
+import time
+import sys
+
+from std_msgs.msg import String
+from move_base_msgs.msg import MoveBaseActionResult
+from actionlib_msgs.msg import GoalStatusArray
+from geometry_msgs.msg import PoseStamped
+
+class MultiGoals:
+    def __init__(self, goalListX, goalListY, retry, map_frame):
+        self.sub = rospy.Subscriber('move_base/result', MoveBaseActionResult, self.statusCB, queue_size=10)
+        self.pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)   
+        # params & variables
+        self.goalListX = goalListX
+        self.goalListY = goalListY
+        self.retry = retry
+        self.goalId = 0
+        self.goalMsg = PoseStamped()
+        self.goalMsg.header.frame_id = map_frame
+        self.goalMsg.pose.orientation.z = 0.0
+        self.goalMsg.pose.orientation.w = 1.0
+        # Publish the first goal
+        time.sleep(1)
+        self.goalMsg.header.stamp = rospy.Time.now()
+        self.goalMsg.pose.position.x = self.goalListX[self.goalId]
+        self.goalMsg.pose.position.y = self.goalListY[self.goalId]
+        self.pub.publish(self.goalMsg) 
+        rospy.loginfo("Initial goal published! Goal ID is: %d", self.goalId) 
+        self.goalId = self.goalId + 1 
+
+    def statusCB(self, data):
+        if data.status.status == 3: # reached
+            self.goalMsg.header.stamp = rospy.Time.now()                
+            self.goalMsg.pose.position.x = self.goalListX[self.goalId]
+            self.goalMsg.pose.position.y = self.goalListY[self.goalId]
+            self.pub.publish(self.goalMsg)  
+            rospy.loginfo("Initial goal published! Goal ID is: %d", self.goalId)              
+            if self.goalId < (len(self.goalListX)-1):
+                self.goalId = self.goalId + 1
+            else:
+                self.goalId = 0 
+                
+
+
+if __name__ == "__main__":
+    try:    
+        # ROS Init    
+        rospy.init_node('multi_goals', anonymous=True)
+
+        # Get params
+        goalListX = rospy.get_param('~goalListX', '[2.0, 2.0]')
+        goalListY = rospy.get_param('~goalListY', '[2.0, 4.0]')
+        map_frame = rospy.get_param('~map_frame', 'map' )
+        retry = rospy.get_param('~retry', '1') 
+
+        goalListX = goalListX.replace("[","").replace("]","")
+        goalListY = goalListY.replace("[","").replace("]","")
+        goalListX = [float(x) for x in goalListX.split(",")]
+        goalListY = [float(y) for y in goalListY.split(",")]
+
+        if len(goalListX) == len(goalListY) & len(goalListY) >=2:          
+            # Constract MultiGoals Obj
+            rospy.loginfo("Multi Goals Executing...")
+            mg = MultiGoals(goalListX, goalListY, retry, map_frame)          
+            rospy.spin()
+        else:
+            rospy.errinfo("Lengths of goal lists are not the same")
+    except KeyboardInterrupt:
+        print("shutting down")
+
+
+
